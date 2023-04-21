0149e73d985642304aca2b45e7c5e90fa347d741
webdevmrinal <mrinal.webdev@gmail.com>
2023-04-21 23:05:53 +0530

    added category page and functionality

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,23 +1,15 @@
 import "./Category.scss";
-import cat1 from "../../assets/category/cat-1.jpg";
+import Products from "./../Products/Products";
 
 const Category = () => {
-    return <div className="shop-by-category">
-        <div className="categories">
-            <div className="category">
-                <img src={cat1} alt="" />
-            </div>
-            <div className="category">
-                <img src={cat1} alt="" />
-            </div>
-            <div className="category">
-                <img src={cat1} alt="" />
-            </div>
-            <div className="category">
-                <img src={cat1} alt="" />
-            </div>
-        </div>
-    </div>;
+  return (
+    <div className="category-main-content">
+      <div className="layout">
+        <div className="category-title">Category Title</div>
+        <Products innerPage={true}/>
+      </div>
+    </div>
+  );
 };
 
 export default Category;
